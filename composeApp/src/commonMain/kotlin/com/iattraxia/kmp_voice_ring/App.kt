a0980b1ee2647a21804a53ea432d9049bf844943
Pablo Vagliati <jvagliat@gmail.com>
2026-04-12 22:44:32 -0300

    View - App: apply safeDrawingPadding to avoid system bars overlap

--- composeApp/src/commonMain/kotlin/com/iattraxia/kmp_voice_ring/App.kt
+++ composeApp/src/commonMain/kotlin/com/iattraxia/kmp_voice_ring/App.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -113,7 +114,8 @@ fun App() {
         BoxWithConstraints(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(0xFF0A0F1C)),
+                .background(Color(0xFF0A0F1C))
+                .safeDrawingPadding(),
         ) {
             val useMobileLayout = isMobilePlatform() || (maxWidth < maxHeight)
 
